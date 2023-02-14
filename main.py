723d9aa1862531780c3180ddf6807f42a1d026ff
llamaair <83296140+llamaair@users.noreply.github.com>
2023-02-14 10:39:39 +0100

    added button

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from tkinter import *
 window=Tk()
 # add widgets here
-
+btn=Button(window, text="Update", fg='blue')
+btn.place(x=80, y=100)
 lbl=Label(window, text="EDM", fg='red', font=("Helvetica", 16))
 lbl.place(x=60, y=50)
 window.title('EDM')
